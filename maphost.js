4c549bc5024dbebfe607f83f48199daa85b1600d
miraklik <130295119+miraklik@users.noreply.github.com>
2023-12-10 15:50:08 +0300

    JavaScriptFile

---
+++ maphost.js
@@ -0,0 +1,7 @@
+import { loadGoogleMapApi } from './loadGoogleMapApi';
+
+const HOST = "https://yandex.ru/maps/?um=constructor%3A8a3543f91752eb49c44f3186cab73564bb2e8ede99ec5edb3b1f09de1ad3e7d5&source=constructorLink";
+const KEY = 'yandex';
+export const URL = 'https://yandex.ru';
+
+  export default loadGoogleMapApi;
